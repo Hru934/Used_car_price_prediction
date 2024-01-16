1c434f778a964774ed78c53153b055b8e0e7250d
Hrutuja Hiwarkar <81550567+Hru934@users.noreply.github.com>
2024-01-16 23:54:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,5 +91,5 @@ def predict():
         return render_template('index.html')
 
 if __name__=="__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
